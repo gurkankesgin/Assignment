e4ee589387ae3fcfe927bce300bbaa685aa3d7c5
gurkan <gurkan.kesgin@kocsistem.com.tr>
2020-06-08 09:05:20 +0300

    ...

--- app/src/main/java/com/turkcell/assignment/data/repositories/ProductRepository.kt
+++ app/src/main/java/com/turkcell/assignment/data/repositories/ProductRepository.kt
@@ -20,6 +20,13 @@ class ProductRepository(
     private val productMapperImpl: ProductMapperImpl
 ) {
 
+    /**
+     *
+     *   Liste ekranı offline first çalışacak şekilde tasarlandı. Detay sayfası ise sadece retrofitin online cache i mevcut.
+     *
+     *   Liste ekranın sadece livedata ile offline databaseden besleniyor. Servisten cevap geldiğinde ise tekrar offline tetiklendiği için ui update oluyor.
+     *
+     */
     private val productList = MutableLiveData<List<Product>>()
 
     init {
